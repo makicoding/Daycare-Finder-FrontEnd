d2005e7e37015f7640cf2668379e30264c34854a
Maki Hirose <makidigital.sync@gmail.com>
2020-01-03 17:07:11 -0500

    More boostrap columns added to search.js

--- src/pages/search.js
+++ src/pages/search.js
@@ -42,34 +42,46 @@ class Search extends React.Component {
                         <Col size="col-md-3">
                             <p>Find Daycare near:</p>
                             <input type="text" className="form-control" id="search-findDaycareNear" onChange={this.handleInputChange}	name="name" placeholder="Town, Address, Zipcode" autoComplete="off"></input>
+                            <br></br>
                         </Col>
 
                         <Col size="col-md-3">
-                          
+                            <p>Opens at or before:</p>
+                            <select className="chosen-select" id="search-opensAtOrBefore" onChange={this.opensAtOrBefore}>
+                                <option value="0730">7:30am</option>
+                                <option value="0800">8:00am</option>
+                                <option value="0830">8:30am</option>
+                                <option value="0900">9:00am</option>
+                            </select>
+                            <br></br>
+                            <br></br>
+                        </Col> 
+
+                        <Col size="col-md-3">
+                            <p>Closes at or after:</p>
+                            <select className="chosen-select" id="search-closesAtOrAfter" onChange={this.closesAtOrAfter}>
+                                <option value="1800">6:00pm</option>
+                                <option value="1830">6:30pm</option>
+                                <option value="1900">7:00pm</option>
+                            </select>
+                            <br></br>
+                            <br></br>
+                        </Col> 
+
+                        <Col size="col-md-3">
+                            <p>Sort by:</p>
+                            <select className="chosen-select" id="search-sortBy" onChange={this.sortBy}>
+                                <option value="1">Open earliest</option>
+                                <option value="2">Close latest</option>
+                            </select>
+                            <br></br>
+                            <br></br>
                         </Col> 
                     </Row>
                 </Container>
 
-                <p>Opens at or before:</p>
-                <select className="chosen-select" id="search-opensAtOrBefore" onChange={this.opensAtOrBefore}>
-                    <option value="0730">7:30am</option>
-                    <option value="0800">8:00am</option>
-                    <option value="0830">8:30am</option>
-                    <option value="0900">9:00am</option>
-                </select>
 
-                <p>Closes at or after:</p>
-                <select className="chosen-select" id="search-closesAtOrAfter" onChange={this.closesAtOrAfter}>
-                    <option value="1800">6:00pm</option>
-                    <option value="1830">6:30pm</option>
-                    <option value="1900">7:00pm</option>
-                </select>
 
-                <p>Sort by:</p>
-                <select className="chosen-select" id="search-sortBy" onChange={this.sortBy}>
-                    <option value="1">Open earliest</option>
-                    <option value="2">Close latest</option>
-                </select>
 
                 <br></br>
                 <br></br>
